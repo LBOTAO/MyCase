dd1ed37fa6ef6822f2d4d46551e07438b15407c0
BDQNLIUTAO <1538286083@qq.com>
2018-05-21 11:58:58 +0800

    快速击键项目第一次上传2018年5月21日11:58:56

---
+++ quickhit/src/cn/happy/Test.java
@@ -0,0 +1,14 @@
+package cn.happy;
+
+/**
+ * @author liutao
+ * @data 2018年5月21日 上午11:55:10
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		Player player = new Player();
+		player.play();
+	}
+
+}
